8b5afc7a687c798d6991af2fc1888c57067abd09
Emre Evcin <emreevcin@icloud.com>
2022-04-20 12:09:15 +0300

    GUI build steps 1

--- src/main/java/com/ce216group1/thecatalog/App.java
+++ src/main/java/com/ce216group1/thecatalog/App.java
@@ -9,8 +9,8 @@ import java.io.IOException;
 
 public class App extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("view.fxml"));
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("view01.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("The Catalog");
         stage.setScene(scene);
--- src/main/java/com/ce216group1/thecatalog/Controller.java
+++ src/main/java/com/ce216group1/thecatalog/Controller.java
@@ -1,4 +1,13 @@
 package com.ce216group1.thecatalog;
 
-public class Controller {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
